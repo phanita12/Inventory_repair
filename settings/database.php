e71371de020452d75819de2ad449a6e43916c970
pachari <pachari_pm@hotmail.com>
2021-09-28 14:55:28 +0700

    edit password

--- settings/database.php
+++ settings/database.php
@@ -5,7 +5,7 @@ return array (
   array (
     'dbdriver' => 'mysql',
     'username' => 'root',
-    'password' => '',
+    'password' => '31M@ssw0rd',
     'dbname' => 'repair',
     'prefix' => 'rp',
     'hostname' => 'localhost',
